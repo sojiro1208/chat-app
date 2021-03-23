b761a8b58c655355ca1ff517e923e9ac6241e8bc
sojiro1208 <asanosouichirou@asanosouichirounoMacBook-Pro.local>
2021-03-23 10:36:22 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,12 @@ ActiveRecord::Schema.define(version: 2021_03_22_115938) do
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -48,4 +52,6 @@ ActiveRecord::Schema.define(version: 2021_03_22_115938) do
 
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
